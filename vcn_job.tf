00b891283a1ba197303e700d123ff6014d9a37bc
Boyang Zhang <boyang.z.zhang@oracle.com>
2022-09-23 13:14:53 -0700

    second job

--- vcn_job.tf
+++ vcn_job.tf
@@ -10,7 +10,7 @@ resource "oci_core_virtual_network" "vcn1" {
   cidr_block = "10.0.0.0/16"
   dns_label = "vcn1"
   compartment_id = "${var.compartment_ocid}"
-  display_name = "${var.display_name_prefix}-VCN - GIT_CONFIG_SOURCE - Rollback Test- Job 1"
+  display_name = "${var.display_name_prefix}-VCN - GIT_CONFIG_SOURCE - Rollback Test- Job 2"
 }
 
 terraform {
