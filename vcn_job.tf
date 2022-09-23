107d3b3bafc2353938cd2cc151cc46848e2f6705
Boyang Zhang <boyang.z.zhang@oracle.com>
2022-09-23 13:11:43 -0700

    first job

---
+++ vcn_job.tf
@@ -0,0 +1,22 @@
+variable "compartment_ocid" {}
+variable "region" {}
+variable "display_name_prefix" {}
+
+provider "oci" {
+  region = "${var.region}"
+}
+
+resource "oci_core_virtual_network" "vcn1" {
+  cidr_block = "10.0.0.0/16"
+  dns_label = "vcn1"
+  compartment_id = "${var.compartment_ocid}"
+  display_name = "${var.display_name_prefix}-VCN - GIT_CONFIG_SOURCE - Rollback Test- Job 1"
+}
+
+terraform {
+  required_providers {
+    oci = {
+      source = "oracle/oci"
+    }
+  }
+}
